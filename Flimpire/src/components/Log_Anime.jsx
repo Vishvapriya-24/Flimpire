2fa91299865352296f7a0475ffb551a6d3628c2a
Vishvapriya-24 <mvishvapriyamurugan@gmail.com>
2025-09-18 19:46:27 +0530

    perfect

--- Flimpire/src/components/Log_Anime.jsx
+++ Flimpire/src/components/Log_Anime.jsx
@@ -1,10 +1,30 @@
-import img from '../assets/black.jpg';
-import styles from '../style/Log_Anime.module.css'
+import { useEffect, useState } from 'react';
+import img1 from '../assets/black.jpg';
+import img2 from '../assets/kaththi.jpg';
+import img3 from '../assets/circle.jpg';
+import styles from '../style/Log_Anime.module.css';
 
 function Log_Anime() {
+    const images = [img1, img2, img3];
+    const [img_index, setimg_Index] = useState(0); // start with first image
+   
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setimg_Index((previmg_Index) => (previmg_Index + 1) % images.length);
+        }, 5000); // change every 3 seconds
+
+        // cleanup function so old timers don't keep running
+        return () => clearInterval(timer);
+    }, []);
+
+
     return (
         <div className={styles.container}>
-            <img src={img} alt="Black background" width="300" />
+            <img 
+                src={images[img_index]} 
+                alt="slideshow" 
+                width="300"
+            />
         </div>
     );
 }
--- Flimpire/src/components/SignIn.jsx
+++ Flimpire/src/components/SignIn.jsx
@@ -1,22 +1,32 @@
 import style from '../style/SignIn.module.css'
-function SignIn({handleSignup}) {
+import { FaCircleUser } from "react-icons/fa6";
+import { TbLockPassword } from "react-icons/tb";
+
+function SignIn({ handleSignup }) {
     return (
         <div className={style.container}>
             <h1 className={style.h1}>Welcome</h1>
             <form id="SignIn" className={style.form}>
-                <fieldset className={style.fieldset}>
-                    <legend className={style.legend}>Email</legend>
-                    <input type="text" className={style.input}/>
-                </fieldset>
-                <fieldset className={style.fieldset}>
-                    <legend className={style.legend}>password </legend>
-                    <input type="text" className={style.input}></input>
-                </fieldset>
+                <div className={style.inform}>
+                    <FaCircleUser className={style.icon} />
+                    <fieldset className={style.fieldset}>
+                        <legend className={style.legend}>Email</legend>
+                        <input type="text" className={style.input} />
+                    </fieldset>
+                </div>
+                <div className={style.inform}>
+                    <TbLockPassword className={style.icon} />
+                    <fieldset className={style.fieldset}>
+                        <legend className={style.legend}>password </legend>
+                        <input type="text" className={style.input}></input>
+                    </fieldset>
+                </div>
+
                 <center>
                     <button className={style.submit}>SignIn</button>
                     <p>Don't have an account? <button className={style.link_signup} onClick={handleSignup}>SignUp</button></p>
                 </center>
-                
+
             </form>
         </div>
     )
