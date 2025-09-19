f9d916901d4235274a4da7ac929498afd79e930c
John Subiksha <subi332006@gmail.com>
2025-09-19 15:52:11 +0530

    Updated SignUp.jsx

--- Flimpire/src/components/SignUp.jsx
+++ Flimpire/src/components/SignUp.jsx
@@ -1,23 +1,33 @@
-import style from '../style/SignIn.module.css'
+import signInStyle from '../style/SignIn.module.css'
+import signUpStyle from '../style/SignUp.module.css'
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
 function SignUp() {
     return (
-        <div className={style.container}>
+        <div className={signUpStyle.container}>
             <h1>SignUp Form</h1>
-            <form id="SignUp">
-                <div>
-                    <input type="text" placeholder='Enter Username'/>
+            <form id="SignUp" className={signUpStyle.form}>
+                <div className={signUpStyle.inputBox}>
+                    <FaUser className={signUpStyle.icon}/>
+                    <input className={signUpStyle.inputStyle} type="text" placeholder='Enter Username'/>
                 </div>
-                <div>
-                    <input type="text" placeholder='Enter Email'/>
+                <div className={signUpStyle.inputBox}>
+                    <MdEmail className={signUpStyle.icon}/>
+                    <input className={signUpStyle.inputStyle} type="text" placeholder='Enter Email'/>
                 </div>
-                <div>
-                    <input type="text" placeholder='Create Password'/>
+                <div className={signUpStyle.inputBox}>
+                    <FaLock className={signUpStyle.icon}/>
+                    <input className={signUpStyle.inputStyle} type="text" placeholder='Create Password'/>
                 </div>
-                <div>
-                    <input type="text" placeholder='Confirm password'/>
+                <div className={signUpStyle.inputBox}>
+                    <FaLock className={signUpStyle.icon}/>
+                    <input className={signUpStyle.inputStyle} type="text" placeholder='Confirm password'/>
                 </div>
-                <button>SignUp</button>
-                <p>Already have an account? <span>SignIn</span></p>
+                <div className={signUpStyle.footer}>
+                    <button className={signUpStyle.button}>SignUp</button>
+                    <p>Already have an account? <span>SignIn</span></p>
+                </div>
             </form>
         </div>
     )
